230573f331d4b45fc37308eca4345f6cf323849d
Elias <eeschmoelz@gmail.com>
2023-10-09 17:46:42 +0100

    chore: demo cleanup

--- apps/web/components/demo/demo.tsx
+++ apps/web/components/demo/demo.tsx
@@ -1,19 +1,11 @@
 'use client';
 
 import { CheckboxGroup, Checkbox } from '@nextui-org/checkbox';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { SplitText, SplitMode } from '@splittext/react';
-import { useSplitText } from '@splittext/react/hooks';
 
 const Demo = () => {
   const [selected, setSelected] = useState(['word']);
-
-  const { scope, getScopedElements, key } = useSplitText();
-
-  useEffect(() => {
-    const elements = getScopedElements();
-    console.log('elements', elements);
-  }, [key]);
 
   return (
     <section className="max-w-md mx-auto text-[#1d1d1d] mt-12">
@@ -21,7 +13,6 @@ const Demo = () => {
       <div className="bg-white p-4 mt-2 rounded-xl">
         <div className="relative resize-x max-w-full min-w-[50%] overflow-hidden">
           <SplitText
-            ref={scope}
             key={selected.join('*')}
             mode={selected as SplitMode[]}
             options={{
@@ -60,7 +51,5 @@ const Demo = () => {
     </section>
   );
 };
-
-// Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis lorem in nisl hendrerit egestas. Donec malesuada convallis magna. Quisque cursus dapibus pulvinar. Praesent consequat metus a nisi ullamcorper, non pulvinar orci fermentum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
 
 export default Demo;
