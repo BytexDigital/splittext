735775c3aa2439ac595db172ed31a10726c73494
Elias <eeschmoelz@gmail.com>
2024-03-11 22:50:51 +0000

    improve children parsing performance

--- packages/splittext-react/src/parse-children.tsx
+++ packages/splittext-react/src/parse-children.tsx
@@ -2,6 +2,95 @@ import React from 'react';
 import { createElement } from './create-element';
 import type { SplitModesOptions } from './types';
 
+// export const parseChildren = (
+//   children: React.ReactNode,
+//   splitLine: boolean,
+//   splitChar: boolean,
+//   componentId: string,
+//   options?: SplitModesOptions,
+//   count?: { current: number },
+// ): React.ReactElement[] => {
+//   const wordCount = count ?? { current: 0 };
+
+//   return React.Children.toArray(children).flatMap((child, index) => {
+//     // Check if this child is a nested react element, and if so, recurse into it
+//     if (React.isValidElement(child)) {
+//       if (splitLine && child.props.children && typeof child.props.children === 'string') {
+//         return child.props.children
+//           .split(/(\n|\r|\p{P}|\s|\w+)/gu)
+//           .filter(Boolean)
+//           .map((word: string, wordIndex: number) => {
+//             return React.createElement(
+//               child.type,
+//               {
+//                 ...child.props,
+//                 key: `${componentId}-${child.type}-${index}-${wordIndex}`,
+//                 'data-str-emb': '',
+//                 'data-str-component': '',
+//               },
+//               parseChildren(word, splitLine, splitChar, componentId, options, wordCount),
+//             );
+//           });
+//       }
+
+//       return React.createElement(
+//         child.type,
+//         {
+//           ...child.props,
+//           key: `${componentId}-${child.type} + ${index}`,
+//           'data-str-emb': splitLine ? '' : undefined,
+//           'data-str-component': '',
+//         },
+//         child.props.children ? parseChildren(child.props.children, splitLine, splitChar, componentId, options, wordCount) : null,
+//       );
+//     }
+
+//     // If it's a string, split it into words, whitespaces and punctuation and wrap each in a div
+//     if (typeof child === 'string') {
+//       return child
+//         .split(/(\n|\r|\p{P}|\s|\w+)/gu)
+//         .filter(Boolean)
+//         .map((word, wordIndex) => {
+//           if (word === '\n' || word === '\r') {
+//             return <br key={`${componentId}-${index}-${wordIndex}`} data-str-component="" />;
+//           }
+
+//           // match whitespace, if user wants to split by line, return an element with a non-breaking space instead
+//           if (word.match(/(\s+)/g)) {
+//             return splitLine ? (
+//               <span key={`${componentId}-${index}-${wordIndex}`} data-str-component="">
+//                 {word}
+//               </span>
+//             ) : (
+//               word
+//             );
+//           }
+
+//           const element = createElement(
+//             options?.word?.tag || 'div',
+//             word,
+//             {
+//               key: `${componentId}-${index}-${wordIndex}`,
+//               splitByChar: splitChar,
+//               splitModeOptions: options,
+//             },
+//             {
+//               style: {
+//                 '--str-word-index': wordCount.current,
+//               },
+//             },
+//           );
+
+//           wordCount.current++;
+//           return element;
+//         });
+//     }
+
+//     // Return null if it's not a string or a valid React element
+//     return null;
+//   });
+// };
+
 export const parseChildren = (
   children: React.ReactNode,
   splitLine: boolean,
@@ -17,8 +106,8 @@ export const parseChildren = (
     if (React.isValidElement(child)) {
       if (splitLine && child.props.children && typeof child.props.children === 'string') {
         return child.props.children
-          .split(/(\n|\r|\p{P}|\s|\w+)/gu)
-          .filter(Boolean)
+          .split(/(\s+|[\w\p{P}]+)/gu)
+          // .filter(Boolean)
           .map((word: string, wordIndex: number) => {
             return React.createElement(
               child.type,
@@ -37,7 +126,7 @@ export const parseChildren = (
         child.type,
         {
           ...child.props,
-          key: `${componentId}-${child.type} + ${index}`,
+          key: `${componentId}-${child.type}-${index}`,
           'data-str-emb': splitLine ? '' : undefined,
           'data-str-component': '',
         },
@@ -45,22 +134,19 @@ export const parseChildren = (
       );
     }
 
-    // If it's a string, split it into words, whitespaces and punctuation and wrap each in a div
+    // If it's a string, split it into words and punctuation combined, and whitespace
     if (typeof child === 'string') {
       return child
-        .split(/(\n|\r|\p{P}|\s|\w+)/gu)
-        .filter(Boolean)
+        .split(/(\s+|[\w\p{P}]+)/gu)
+        .filter(part => part.length > 0)
         .map((word, wordIndex) => {
           if (word === '\n' || word === '\r') {
             return <br key={`${componentId}-${index}-${wordIndex}`} data-str-component="" />;
           }
 
-          // match whitespace, if user wants to split by line, return an element with a non-breaking space instead
-          if (word.match(/(\s+)/g)) {
+          if (word.match(/^\s*$/)) {
             return splitLine ? (
-              <span key={`${componentId}-${index}-${wordIndex}`} data-str-component="">
-                {word}
-              </span>
+              <span key={`${componentId}-${index}-${wordIndex}`} data-str-component="">{word}</span>
             ) : (
               word
             );
@@ -90,3 +176,4 @@ export const parseChildren = (
     return null;
   });
 };
+
