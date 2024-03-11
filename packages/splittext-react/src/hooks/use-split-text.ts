a86ad14038372990af890eab40931afdd9b28159
Elias <eeschmoelz@gmail.com>
2024-03-11 22:48:11 +0000

    refactor useSplitHooks generics

--- packages/splittext-react/src/hooks/use-split-text.ts
+++ packages/splittext-react/src/hooks/use-split-text.ts
@@ -2,38 +2,37 @@
 
 import React from 'react';
 import { useConstant } from '../utils';
-import { SplitElements } from '../types';
 
-interface SplitTextScope<T extends Element> {
-  readonly current: T;
+export interface SplitTextScope<T extends React.ElementRef<any> = React.ElementRef<'div'>> extends React.RefObject<T> {
   _trigger: () => void;
 }
 
+export type SplitElements = ReturnType<ReturnType<typeof createScopedElements>>;
+
 const createScopedElements = <
-  ScopeCurrentElementType extends Element = Element,
   C extends Element = HTMLDivElement,
   W extends Element = HTMLDivElement,
-  L extends Element = HTMLDivElement,
+  L extends Element = HTMLDivElement
 >(
-  scope: SplitTextScope<ScopeCurrentElementType>,
+  scope: SplitTextScope,
 ) => {
-  return (): SplitElements<C, W, L> => {
+  return () => {
     const { current } = scope;
     return {
-      chars: current?.querySelectorAll('[data-str-type=char]'),
-      words: current?.querySelectorAll('[data-str-type=word]'),
-      lines: current?.querySelectorAll('[data-str-type=line]'),
+      chars: current?.querySelectorAll<C>('[data-str-type=char]'),
+      words: current?.querySelectorAll<W>('[data-str-type=word]'),
+      lines: current?.querySelectorAll<L>('[data-str-type=line]'),
     };
   };
 };
 
-export function useSplitText<T extends Element = any>() {
+export function useSplitText() {
   const [key, setKey] = React.useState(0);
 
   const triggerRender = () => setKey((prev) => prev + 1);
 
-  const scope: SplitTextScope<T> = useConstant(() => ({
-    current: null!,
+  const scope = useConstant(() => ({
+    current: null,
     _trigger: triggerRender,
   }));
 
