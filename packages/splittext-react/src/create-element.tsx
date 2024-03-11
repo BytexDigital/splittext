c03cedc7ef9e803021f42af4dce7f7345306dfa9
Elias <eeschmoelz@gmail.com>
2024-03-11 22:51:12 +0000

    formatting

--- packages/splittext-react/src/create-element.tsx
+++ packages/splittext-react/src/create-element.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SplitModesOptions } from './types';
+import type { SplitModesOptions } from './types';
 
 interface CustomProps<T> extends React.HTMLAttributes<T> {
   [key: string]: any;
@@ -35,9 +35,8 @@ export const createElement: any = (
         ...overrides?.style,
       },
       key: `str-word-${options.key}`,
-      className: `str-word ${options?.props?.className?.replace('str-word', '') || ''} ${
-        options?.splitModeOptions?.word?.className || ''
-      }`,
+      className: `str-word ${options?.props?.className?.replace('str-word', '') || ''} ${options?.splitModeOptions?.word?.className || ''
+        }`,
       'data-str-type': 'word',
       'data-str-component': '',
       'data-str-data': children,
@@ -45,43 +44,43 @@ export const createElement: any = (
     // check if user wants to split word by char
     options?.splitByChar
       ? children?.split('').map((char, charIndex) => {
-          const baseChild = React.createElement(
-            options?.splitModeOptions?.char?.tag || 'div',
-            {
-              key: `str-char-${options.key}-${charIndex}-${char}`,
-              'data-str-type': 'char',
-              'data-str-data': char,
-              style: {
-                ...options?.splitModeOptions?.char?.style,
-                ...defaultProps.style,
-                '--str-char-index': charIndex,
-              } as React.CSSProperties,
-              className: `str-char ${options?.splitModeOptions?.char?.className || ''}`,
-            },
-            char,
-          );
+        const baseChild = React.createElement(
+          options?.splitModeOptions?.char?.tag || 'div',
+          {
+            key: `str-char-${options.key}-${charIndex}-${char}`,
+            'data-str-type': 'char',
+            'data-str-data': char,
+            style: {
+              ...options?.splitModeOptions?.char?.style,
+              ...defaultProps.style,
+              '--str-char-index': charIndex,
+            } as React.CSSProperties,
+            className: `str-char ${options?.splitModeOptions?.char?.className || ''}`,
+          },
+          char,
+        );
 
-          // check if user wants to wrap each char in a div
-          return options?.splitModeOptions?.char?.wrapper ||
-            options?.splitModeOptions?.char?.wrapperProps?.className ||
-            options?.splitModeOptions?.char?.wrapperProps?.style
-            ? React.createElement(
-                'div',
-                {
-                  key: `str-wrapper-char-${options.key}-${charIndex}`,
-                  'data-str-data': char,
-                  className: options?.splitModeOptions?.char?.wrapperProps?.className,
-                  style: {
-                    overflow: 'hidden',
-                    verticalAlign: 'top',
-                    ...options?.splitModeOptions?.char?.wrapperProps?.style,
-                    display: 'inline-block',
-                  },
-                },
-                baseChild,
-              )
-            : baseChild;
-        })
+        // check if user wants to wrap each char in a div
+        return options?.splitModeOptions?.char?.wrapper ||
+          options?.splitModeOptions?.char?.wrapperProps?.className ||
+          options?.splitModeOptions?.char?.wrapperProps?.style
+          ? React.createElement(
+            'div',
+            {
+              key: `str-wrapper-char-${options.key}-${charIndex}`,
+              'data-str-data': char,
+              className: options?.splitModeOptions?.char?.wrapperProps?.className,
+              style: {
+                overflow: 'hidden',
+                verticalAlign: 'top',
+                ...options?.splitModeOptions?.char?.wrapperProps?.style,
+                display: 'inline-block',
+              },
+            },
+            baseChild,
+          )
+          : baseChild;
+      })
       : children,
   );
 
@@ -90,19 +89,19 @@ export const createElement: any = (
     options?.splitModeOptions?.word?.wrapperProps?.className ||
     options?.splitModeOptions?.word?.wrapperProps?.style
     ? React.createElement(
-        'div',
-        {
-          key: `str-wrapper-word-${options.key}`,
-          'data-str-data': children,
-          className: options?.splitModeOptions?.word?.wrapperProps?.className,
-          style: {
-            overflow: 'hidden',
-            verticalAlign: 'top',
-            ...options?.splitModeOptions?.word?.wrapperProps?.style,
-            display: 'inline-block',
-          },
+      'div',
+      {
+        key: `str-wrapper-word-${options.key}`,
+        'data-str-data': children,
+        className: options?.splitModeOptions?.word?.wrapperProps?.className,
+        style: {
+          overflow: 'hidden',
+          verticalAlign: 'top',
+          ...options?.splitModeOptions?.word?.wrapperProps?.style,
+          display: 'inline-block',
         },
-        baseComponent,
-      )
+      },
+      baseComponent,
+    )
     : baseComponent;
 };
