aeafe11a214ed782c8eac1f733e4160442278dd6
Elias <eeschmoelz@gmail.com>
2024-03-11 22:49:58 +0000

    improve accessibility, refactor splittext props

--- packages/splittext-react/src/split-text.tsx
+++ packages/splittext-react/src/split-text.tsx
@@ -5,166 +5,155 @@ import { createLines } from './create-lines';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { immediateDebounce } from './utils';
-import './styles.css';
-import { SplitElements, SplitMode, SplitModesOptions } from './types';
+import type { SplitMode, SplitModesOptions } from './types';
+import type { SplitElements, SplitTextScope } from './hooks/use-split-text';
 
-type Merge<P1 = {}, P2 = {}> = Omit<P1, keyof P2> & P2;
+type SplitTextProps<T extends React.ElementType> = Omit<React.ComponentProps<T>, 'as' | "mode" | "resizeDebounce" | "options" | "onComplete" | "onResize"> & {
+  children: React.ReactNode;
+  as?: T;
+  mode?: SplitMode | SplitMode[];
+  resizeDebounce?: number;
+  options?: SplitModesOptions;
+  onComplete?: (elements: SplitElements) => void;
+  onResize?: (elements: SplitElements) => void;
+};
 
-type MergeProps<E, P = {}> = P & Merge<E extends React.ElementType ? React.ComponentPropsWithRef<E> : never, P>;
-
-interface ForwardRefComponent<IntrinsicElementString, OwnProps = {}>
-  extends React.ForwardRefExoticComponent<
-    MergeProps<
-      IntrinsicElementString,
-      OwnProps & {
-        as?: IntrinsicElementString;
-      }
-    >
-  > {
-  <As extends keyof JSX.IntrinsicElements>(props: MergeProps<As, OwnProps & { as: As }>): React.ReactElement | null;
-  <
-    As extends React.ElementType<any>,
-    _AsWithProps = As extends React.ElementType<infer P> ? React.ElementType<P> : never,
-  >(
-    props: MergeProps<_AsWithProps, OwnProps & { as: _AsWithProps }>,
-  ): React.ReactElement | null;
+function internalForwardRef<T, P = {}>(
+  render: (props: P, ref: React.Ref<T>) => React.ReactNode
+): (props: P & React.RefAttributes<T>) => React.ReactNode {
+  return React.forwardRef(render) as any;
 }
 
-type SplitTextProps = ForwardRefComponent<
-  'div',
-  {
-    children: React.ReactNode;
-    mode?: SplitMode | SplitMode[];
-    resizeDebounce?: number;
-    options?: SplitModesOptions;
-    onComplete?: (elements: SplitElements) => void;
-    onResize?: (elements: SplitElements) => void;
-  }
->;
+const InternalSplitText = <T extends React.ElementType = 'div'>(
+  { children, as, options, onComplete, onResize, mode = 'word', resizeDebounce = 500, ...props }: SplitTextProps<T>,
+  ref: React.ForwardedRef<T>,
+) => {
+  const Component = as || 'div';
+  const id = React.useId();
 
-const SplitTextBase = React.forwardRef(
-  (
-    { as: Component = 'div', mode = 'word', resizeDebounce = 500, options, onComplete, onResize, ...props },
-    forwardedRef,
-  ) => {
-    const id = React.useId();
+  // container refs
+  const containerRef = React.useRef<HTMLDivElement | null>(null);
+  const internalRef = React.useRef<React.ElementRef<typeof Component> | null>(null);
 
-    const ref = React.useRef<React.ElementRef<typeof Component> | null>(null);
+  // resize observer
+  const resizeObserver = React.useRef<ResizeObserver | null>(null);
+  const resizeObserverContentRect = React.useRef<DOMRectReadOnly | undefined>(undefined);
 
-    // resize observer
-    const resizeObserver = React.useRef<ResizeObserver | null>(null);
-    const resizeObserverContentRect = React.useRef<DOMRectReadOnly | undefined>(undefined);
+  // track if text has been split
+  const isSplit = React.useRef(false);
 
-    // has text been split
-    const isSplit = React.useRef(false);
+  // track initial render
+  const initialRender = React.useRef(true);
 
-    // initial render
-    const initialRender = React.useRef(true);
+  // track if onComplete callback has been called
+  const hasOnCompleteBeenCalled = React.useRef(false);
 
-    // has onComplete Callback been called
-    const onCompleteCalled = React.useRef(false);
+  // state to hold the split text elements (lines, words, chars) and make it easy to trigger rerender
+  const [elements, setElements] = React.useState<Array<React.ReactElement>>([]);
 
-    // state to hold the split text elements (lines, words, chars) and make it easy to trigger rerender
-    const [elements, setElements] = React.useState<Array<React.ReactElement>>([]);
+  // modes to split the text into
+  const modes = Array.isArray(mode) ? mode : [mode];
+  const splitByLine = modes.includes('line');
+  const splitByChar = modes.includes('char');
 
-    // modes to split the text into
-    const modes = Array.isArray(mode) ? mode : [mode];
-    const hasLineMode = modes.includes('line');
-    const hasCharMode = modes.includes('char');
+  React.useImperativeHandle(ref as React.Ref<T>, () => internalRef.current as T, []);
 
-    // expose container ref to forwarded ref
-    //@ts-ignore
-    React.useImperativeHandle(forwardedRef, () => ref.current);
+  const handleNodeRef = React.useCallback((node: HTMLDivElement | null) => {
+    containerRef.current = node;
 
-    const handleNodeRef = React.useCallback((node: React.ElementRef<typeof Component>) => {
-      ref.current = node;
+    // setup resize observer to split text by lines on container parent resize if user has line mode enabled
+    if (splitByLine && node) {
+      const split = (e: ResizeObserverEntry[]) => {
+        if (!containerRef.current) return;
 
-      // setup resize observer to split text by lines on container parent resize if user has line mode enabled
-      if (hasLineMode && node) {
-        const split = (e: ResizeObserverEntry[]) => {
-          if (!ref.current) return;
-
-          // check if resizeobserverentry contentRect has changed since last resize observer event and exit if not to prevent unnecessary rerenders
-          if (!resizeObserverContentRect.current) {
-            resizeObserverContentRect.current = e[0]?.contentRect;
-          } else {
-            if (
-              resizeObserverContentRect.current.width === e[0]?.contentRect.width
-              //resizeObserverContentRect.current.height === e[0]?.contentRect.height
-            ) {
-              return;
-            }
-
-            resizeObserverContentRect.current = e[0]?.contentRect;
+        // check if resizeobserverentry contentRect has changed since last resize observer event and exit if not to prevent unnecessary rerenders
+        if (!resizeObserverContentRect.current) {
+          resizeObserverContentRect.current = e[0]?.contentRect;
+        } else {
+          if (
+            resizeObserverContentRect.current.width === e[0]?.contentRect.width
+            //resizeObserverContentRect.current.height === e[0]?.contentRect.height
+          ) {
+            return;
           }
 
-          // split text into lines
-          const splitElements = createLines(ref.current, hasCharMode, isSplit.current, id, options);
-
-          if (!isSplit.current) {
-            isSplit.current = true;
-          }
-
-          ReactDOM.flushSync(() => {
-            setElements(splitElements);
-          });
-        };
-
-        // debounce resize handler
-        const debouncedSplit = immediateDebounce(split, resizeDebounce);
-
-        //resize observer to split text on container parent resize
-        resizeObserver.current = new ResizeObserver(debouncedSplit);
-        resizeObserver.current.observe(ref.current?.parentElement!);
-      }
-
-      // disconnect resize observer when component is unmounted
-      if (!node) {
-        resizeObserver.current?.disconnect();
-      }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    React.useEffect(() => {
-      if (!ref.current) return;
-
-      if (initialRender.current && hasLineMode) {
-        initialRender.current = false;
-        return;
-      }
-
-      if (onComplete !== undefined || onResize !== undefined) {
-        const elements: SplitElements = {
-          chars: (hasCharMode && ref.current.querySelectorAll("[data-str-type='char']")) || undefined,
-          words: ref.current.querySelectorAll("[data-str-type='word']"),
-          lines: (hasLineMode && ref.current.querySelectorAll("[data-str-type='line']")) || undefined,
-        };
-
-        if (!onCompleteCalled.current) {
-          onCompleteCalled.current = true;
-          onComplete?.(elements);
+          resizeObserverContentRect.current = e[0]?.contentRect;
         }
 
-        if (hasLineMode) {
-          onResize?.(elements);
+        // split text into lines
+        const splitElements = createLines(internalRef.current, splitByChar, isSplit.current, id, options);
+
+        if (!isSplit.current) {
+          isSplit.current = true;
         }
+
+        ReactDOM.flushSync(() => {
+          setElements(splitElements);
+        });
+      };
+
+      // debounce resize handler
+      const debouncedSplit = immediateDebounce(split, resizeDebounce);
+
+      //resize observer to split text on container parent resize
+      resizeObserver.current = new ResizeObserver(debouncedSplit);
+      resizeObserver.current.observe(containerRef.current!);
+    }
+
+    // disconnect resize observer when component is unmounted
+    if (!node) {
+      resizeObserver.current?.disconnect();
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if (!internalRef.current) return;
+
+    // skip initial render if split by line
+    if (initialRender.current && splitByLine) {
+      initialRender.current = false;
+      return;
+    }
+
+    // if onComplete or onResize callbacks are provided, call them with the split elements as arguments
+    if (onComplete !== undefined || onResize !== undefined) {
+      const elements = {
+        chars: (splitByChar && (internalRef.current as unknown as Element).querySelectorAll("[data-str-type='char']")) || undefined,
+        words: (internalRef.current as unknown as Element).querySelectorAll("[data-str-type='word']"),
+        lines: (splitByLine && (internalRef.current as unknown as Element).querySelectorAll("[data-str-type='line']")) || undefined,
+      } satisfies SplitElements;
+
+      if (!hasOnCompleteBeenCalled.current) {
+        hasOnCompleteBeenCalled.current = true;
+        onComplete?.(elements);
       }
 
-      if (hasLineMode && forwardedRef && '_trigger' in forwardedRef) {
-        (forwardedRef as { _trigger?: () => void })._trigger?.();
+      if (splitByLine) {
+        onResize?.(elements);
       }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [elements]);
+    }
 
-    return (
-      <Component {...props} ref={handleNodeRef}>
-        {/* <span className="sr-only">{props.children}</span> */}
-        {elements.length > 0 ? elements : parseChildren(props.children, hasLineMode, hasCharMode, id, options)}
-      </Component>
-    );
-  },
-) as SplitTextProps;
+    if (splitByLine && ref && '_trigger' in ref) {
+      (ref as SplitTextScope<T>)._trigger?.();
+    }
+  }, [elements]);
 
-SplitTextBase.displayName = 'SplitText';
+  return (
+    <div data-str-wrapper='' ref={handleNodeRef} style={{ position: "relative" }}>
+      <span style={{
+        position: "absolute",
+        width: 1,
+        height: 1,
+        padding: 0,
+        margin: -1,
+        overflow: "hidden",
+        clip: "rect(0, 0, 0, 0)",
+        whiteSpace: "nowrap",
+        borderWidth: 0,
+      }}>{children}</span>
+      {React.createElement(Component, { ...props, 'data-str-root': '', "aria-hidden": true, ref: internalRef }, elements.length > 0 ? elements : parseChildren(children, splitByLine, splitByChar, id, options))}
+    </div>
+  )
+};
 
-export const SplitText = React.memo(SplitTextBase, () => true);
+export const SplitText = internalForwardRef(InternalSplitText);
